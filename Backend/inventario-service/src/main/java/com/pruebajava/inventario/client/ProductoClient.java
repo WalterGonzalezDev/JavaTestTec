9a2b608fb37bdfa7c8b9b426c2be8536dd682f1d
WalterGonzalezDev <waltergonzalezdeveloper@gmail.com>
2025-11-03 15:35:36 -0500

    Se agrega API Key

--- Backend/inventario-service/src/main/java/com/pruebajava/inventario/client/ProductoClient.java
+++ Backend/inventario-service/src/main/java/com/pruebajava/inventario/client/ProductoClient.java
@@ -2,6 +2,9 @@ package com.pruebajava.inventario.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,10 +17,17 @@ public class ProductoClient {
     @Value("${servicios.productos.url}")
     private String productosServiceUrl;
     
+    @Value("${servicios.productos.api-key}")
+    private String productosApiKey;
+    
     public boolean existeProducto(String sku) {
         try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-API-Key", productosApiKey);
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+            
             String url = productosServiceUrl + "/api/productos/sku/" + sku;
-            restTemplate.getForObject(url, Object.class);
+            restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
             return true;
         } catch (Exception e) {
             return false;
