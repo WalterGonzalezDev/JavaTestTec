6c783508b700a9903d8024b95718dbf3f13b692b
WalterGonzalezDev <waltergonzalezdeveloper@gmail.com>
2025-11-03 13:14:52 -0500

    Configuración JWT

---
+++ Backend/inventario-service/src/main/java/com/pruebajava/inventario/security/JwtProperties.java
@@ -0,0 +1,27 @@
+package com.pruebajava.inventario.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "security.jwt")
+public class JwtProperties {
+    private String secretKey;
+    private long expiration;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public long getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(long expiration) {
+        this.expiration = expiration;
+    }
+}
